9f66f3587aef8ce42f351dec5facd739408321d7
Daniel Rodriguez <daniel.rguez.hdez@gmail.com>
2016-09-18 11:26:51 +0200

    Add travis support

---
+++ samples/regions/src/test/java/com/kaas/samples/regions/specs/RegionsAppSpec.groovy
@@ -0,0 +1,26 @@
+package com.kaas.samples.regions.specs
+
+import groovyx.net.http.RESTClient
+import spock.lang.Specification
+
+/**
+ * Created by drodriguez on 18/9/16.
+ */
+class RegionsAppSpec extends Specification {
+
+    RESTClient restClient = new RESTClient("http://localhost:8080")
+
+    def "Obtain provinces of Madrid county" () {
+        given:
+        String county = "Madrid"
+
+        when:
+        def response = restClient.get(path: '/regions' , query: ['q' : county])
+
+        then:
+        response.status == 200
+
+        and:
+        response.responseData.name == "Madrid"
+    }
+}
