2074a0367e832f5f50ba1432f0e8a4b38cb0735f
Daniel Rodriguez <daniel.rguez.hdez@gmail.com>
2016-09-20 16:32:24 +0200

    Remove unnecesary comments

--- samples/regions/src/test/java/com/kaas/samples/regions/it/RegionsAppTest_IT.java
+++ samples/regions/src/test/java/com/kaas/samples/regions/it/RegionsAppTest_IT.java
@@ -3,7 +3,6 @@ package com.kaas.samples.regions.it;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//Comentario
 @RunWith(SpringJUnit4ClassRunner.class)
 public class RegionsAppTest_IT {
 }
