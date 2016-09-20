6e7bc63b86009366d068af80fe816ff541c145c5
Daniel Rodriguez <daniel.rguez.hdez@gmail.com>
2016-09-20 13:50:32 +0200

    Adding integration tests support for Spring.

--- samples/regions/src/test/java/com/kaas/samples/regions/it/RegionsAppTest_IT.java
+++ samples/regions/src/test/java/com/kaas/samples/regions/it/RegionsAppTest_IT.java
@@ -3,6 +3,7 @@ package com.kaas.samples.regions.it;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+//Comentario
 @RunWith(SpringJUnit4ClassRunner.class)
 public class RegionsAppTest_IT {
 }
